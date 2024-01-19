5b0b25d5bdb42256305e17fdaa79faa691dfbde8
orpos <58054927+orpos@users.noreply.github.com>
2024-01-19 18:50:45 -0300

    fix app crashing when using pronto code (not tested)

--- android/src/main/java/com/danielr18/irmanager/RNIRManagerModule.java
+++ android/src/main/java/com/danielr18/irmanager/RNIRManagerModule.java
@@ -28,76 +28,79 @@ public class RNIRManagerModule extends ReactContextBaseJavaModule {
     return MODULE_NAME;
   }
 
-    @ReactMethod
-    public void hasIrEmitter(Promise promise) {
-        promise.resolve(manager.hasIrEmitter());
+  @ReactMethod
+  public void hasIrEmitter(Promise promise) {
+    promise.resolve(manager.hasIrEmitter());
+  }
+
+  @ReactMethod
+  public void getCarrierFrequencies(Promise promise) {
+    try {
+      CarrierFrequencyRange[] carrierFrequencyRanges = manager.getCarrierFrequencies();
+      WritableArray carrierFrequencies = Arguments.createArray();
+
+      for (CarrierFrequencyRange carrierFrequencyRange : carrierFrequencyRanges) {
+        WritableMap carrierFrequency = Arguments.createMap();
+        carrierFrequency.putInt("minFrequency", carrierFrequencyRange.getMinFrequency());
+        carrierFrequency.putInt("maxFrequency", carrierFrequencyRange.getMaxFrequency());
+        carrierFrequencies.pushMap(carrierFrequency);
+      }
+
+      promise.resolve(carrierFrequencies);
+    } catch (Exception e) {
+      promise.reject(e);
     }
 
-    @ReactMethod
-    public void getCarrierFrequencies(Promise promise) {
-        try {
-            CarrierFrequencyRange[] carrierFrequencyRanges = manager.getCarrierFrequencies();
-            WritableArray carrierFrequencies = Arguments.createArray();
+  }
 
-            for (CarrierFrequencyRange carrierFrequencyRange : carrierFrequencyRanges) {
-                WritableMap carrierFrequency = Arguments.createMap();
-                carrierFrequency.putInt("minFrequency", carrierFrequencyRange.getMinFrequency());
-                carrierFrequency.putInt("maxFrequency", carrierFrequencyRange.getMaxFrequency());
-                carrierFrequencies.pushMap(carrierFrequency);
-            }
+  @ReactMethod
+  public void transmit(Integer carrierFrequency, ReadableArray burstsPattern, Promise promise) {
+    int[] pattern = new int[burstsPattern.size()];
 
-            promise.resolve(carrierFrequencies);
-        } catch (Exception e) {
-            promise.reject(e);
-        }
-
+    for (int i = 0; i < burstsPattern.size(); i++) {
+      pattern[i] = burstsPattern.getInt(i);
     }
 
-    @ReactMethod
-    public void transmit(Integer carrierFrequency, ReadableArray burstsPattern, Promise promise) {
-        int[] pattern = new int[burstsPattern.size()];
-
-        for (int i = 0; i < burstsPattern.size(); i++) {
-            pattern[i] = burstsPattern.getInt(i);
-        }
-
-        try {
-            manager.transmit(carrierFrequency, pattern);
-            promise.resolve(true);
-        } catch (Exception e) {
-            promise.reject(e);
-        }
+    try {
+      manager.transmit(carrierFrequency, pattern);
+      promise.resolve(true);
+    } catch (Exception e) {
+      promise.reject(e);
     }
+  }
 
-    @ReactMethod
-    public void transmitProntoCode(String prontoHexCode, Promise promise) {
-        String[] codeParts = prontoHexCode.split(" ");
+  @ReactMethod
+  public void transmitProntoCode(String prontoHexCode, Promise promise) {
+    try {
+      String[] codeParts = prontoHexCode.split(" ");
 
-        int prontoClockFrequency = Integer.parseInt(codeParts[1], 16);
-        Double exactCarrierFrequency = 1000000/(prontoClockFrequency * 0.241246);
+      int prontoClockFrequency = Integer.parseInt(codeParts[1], 16);
+      Double exactCarrierFrequency = 1000000/(prontoClockFrequency * 0.241246);
 
-        int carrierFrequency = exactCarrierFrequency.intValue();
-        int firstSequenceBurstPairs = Integer.parseInt(codeParts[2], 16);
-        int secondSequenceBurstPairs = Integer.parseInt(codeParts[3], 16);
-        int[] pattern = new int[(firstSequenceBurstPairs * 2) + (secondSequenceBurstPairs * 2)];
+      int carrierFrequency = exactCarrierFrequency.intValue();
+      int firstSequenceBurstPairs = Integer.parseInt(codeParts[2], 16);
+      int secondSequenceBurstPairs = Integer.parseInt(codeParts[3], 16);
+      int[] pattern = new int[(firstSequenceBurstPairs * 2) + (secondSequenceBurstPairs * 2)];
 
-        int i = 0;
-        int firstPairIndex = 4;
-        int secondPairIndex = firstPairIndex + (firstSequenceBurstPairs * 2);
+      int i = 0;
+      int firstPairIndex = 4;
+      int secondPairIndex = firstPairIndex + (firstSequenceBurstPairs * 2);
 
-        for (int j = firstPairIndex; j < secondPairIndex; i++, j++) {
-            pattern[i] = Integer.parseInt(codeParts[j], 16) * (1000000 / carrierFrequency);
-        }
+      for (int j = firstPairIndex; j < secondPairIndex; i++, j++) {
+        pattern[i] = Integer.parseInt(codeParts[j], 16) * (1000000 / carrierFrequency);
+      }
 
-        for (int j = secondPairIndex; j < secondPairIndex + (secondSequenceBurstPairs * 2); i++, j++) {
-            pattern[i] = Integer.parseInt(codeParts[j], 16) * (1000000 / carrierFrequency);
-        }
-
-        try {
-            manager.transmit(carrierFrequency, pattern);
-            promise.resolve(true);
-        } catch (Exception e) {
-            promise.reject(e);
-        }
+      for (int j = secondPairIndex; j < secondPairIndex + (secondSequenceBurstPairs * 2); i++, j++) {
+        pattern[i] = Integer.parseInt(codeParts[j], 16) * (1000000 / carrierFrequency);
+          }
+    }catch (Exception e) {
+      promise.reject(e);
     }
+    try {
+      manager.transmit(carrierFrequency, pattern);
+      promise.resolve(true);
+    } catch (Exception e) {
+      promise.reject(e);
+    }
+  }
 }
